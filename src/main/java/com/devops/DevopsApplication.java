502c228eb413d570f673f2a632b2c81e0f9dcfae
marciodasilva <marciodasilva@hotmail.com>
2016-10-16 09:21:52 -0700

    Externalize webmaster login and password

--- src/main/java/com/devops/DevopsApplication.java
+++ src/main/java/com/devops/DevopsApplication.java
@@ -10,6 +10,7 @@ import com.devops.utils.UsersUtils;
 import org.apache.log4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -27,18 +28,27 @@ public class DevopsApplication implements CommandLineRunner {
 	@Autowired
 	private UserService userService;
 
+	@Value("${webmaster.username}")
+	private String webmasterUsername;
+
+	@Value("${webmaster.password}")
+	private String webmasterPassword;
+
+	@Value("${webmaster.email}")
+	private String webmasterEmail;
+
 	public static void main(String[] args) {
 		SpringApplication.run(DevopsApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		String username = "marciodasilva";
-		String email = "marciodasilva@hotmail.com";
-		User user = UsersUtils.createBasicUser(username, email);
+
+		User user = UsersUtils.createBasicUser(webmasterUsername, webmasterEmail);
+		user.setPassword(webmasterPassword);
 
 		Set<UserRole> userRoles = new HashSet<>();
-		userRoles.add(new UserRole(user, new Role(RolesEnum.PRO)));
+		userRoles.add(new UserRole(user, new Role(RolesEnum.ADMIN)));
 		LOG.debug("Creating user with username {}", user.getUsername());
 		userService.createUser(user, PlansEnum.PRO, userRoles);
 		LOG.info("User {} created", user.getUsername());
--- src/main/java/com/devops/config/ApplicationConfig.java
+++ src/main/java/com/devops/config/ApplicationConfig.java
@@ -2,6 +2,7 @@ package com.devops.config;
 
 import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -12,5 +13,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableJpaRepositories(basePackages = "com.devops.backend.persistence.repository")
 @EntityScan(basePackages = "com.devops.backend.persistence.domain.backend")
 @EnableTransactionManagement
+@PropertySource("file:///${user.home}/.devops/application-common.properties")
 public class ApplicationConfig {
 }
